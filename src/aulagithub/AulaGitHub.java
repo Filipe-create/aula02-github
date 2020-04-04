6b34bf98597a5853e5425f88eeed2fdfc3215ce4
Filipe-create <filipe82645@gmail.com>
2020-04-04 14:17:37 -0300

    Acrescentada a mensagem de Boa Tarde.

--- src/aulagithub/AulaGitHub.java
+++ src/aulagithub/AulaGitHub.java
@@ -6,6 +6,7 @@ public class AulaGitHub {
     public static void main(String[] args) {
         
         System.out.println("Boa noite!");
+        System.out.println("Boa tarde!");
     }
     
 }
